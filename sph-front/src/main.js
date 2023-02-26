5d5ca140e05c2232470dc162d2671a4a1afe3cbb
liberty-code <1405333434@qq.com>
2023-02-26 23:04:11 +0800

    feat：end

--- sph-front/src/main.js
+++ sph-front/src/main.js
@@ -11,7 +11,7 @@ import * as API from '@/api'
 import TypeNav from '@/components/TypeNav'
 import Carousel from '@/components/Carousel'
 import Pagination from '@/components/Pagination'
-import {Button,MessageBox} from 'element-ui'
+import {Button, MessageBox} from 'element-ui'
 
 Vue.component(TypeNav.name, TypeNav)
 Vue.component(Carousel.name, Carousel)
@@ -23,16 +23,19 @@ Vue.prototype.$alert = MessageBox.alert
 Vue.config.productionTip = false
 
 import atm from '@/assets/images/1.gif'
-Vue.use(VueLazyload,{
+
+Vue.use(VueLazyload, {
   loading: atm
 })
 
 import myPlugins from '@/plugins/myPlugins'
-Vue.use(myPlugins,{
+
+Vue.use(myPlugins, {
   name: 'upper'
 })
 
 import '@/plugins/validate'
+
 new Vue({
   render: (h) => h(App),
   beforeCreate() {
