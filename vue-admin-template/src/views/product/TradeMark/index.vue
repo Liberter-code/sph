5a89242f549a81d2699148e0a3c19dbb98b9ab9b
liberty-code <1405333434@qq.com>
2023-02-28 21:55:10 +0800

    feat：后台管理系统

---
+++ vue-admin-template/src/views/product/TradeMark/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <el-button type="primary" icon="el-icon-plus" size="small">添加</el-button>
+    <el-table border>
+      <el-table-column></el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TradeMark'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
